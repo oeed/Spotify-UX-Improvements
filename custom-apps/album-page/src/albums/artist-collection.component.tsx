212d609ec469d0f67442d1143dd0c9991e10110a
Oliver Cooper <oliver.cooper@me.com>
2019-12-12 15:13:53 +1300

    Added random album button

--- custom-apps/album-page/src/albums/artist-collection.component.tsx
+++ custom-apps/album-page/src/albums/artist-collection.component.tsx
@@ -1,6 +1,8 @@
 import AlbumItem from "albums/album-item.component";
 import { alphabetScrollerAnchor } from "misc/alphabet-scroller.component";
-import React, { Component } from "react";
+import { playAlbum } from "player.helper";
+import React, { Component, MouseEvent } from "react";
+import { getAlbums } from "shared/data.helper";
 import { ArtistAlbums } from "./album-list.component";
 import { getArtistCharacter } from "./album.helper";
 
@@ -17,13 +19,22 @@ export default class ArtistCollection extends Component<ArtistCollectionProps> {
         <a className="ap-alphabet-link" id={ alphabetScrollerAnchor(getArtistCharacter(artist)) }/>
       }
 
-      <div className="ap-artist-collection" >
-        <h1>{ artist.name }</h1>
+      <div className="ap-artist-collection">
+        <div className="ap-artist-header">
+          <h1>{ artist.name }</h1>
+          <button className="spoticon-shuffle-16" onClick={ this.onRandomAlbumClick }/>
+        </div>
         <div className="ap-albums">
           { albums.map((album, i) => <AlbumItem key={ i } album={ album }/> )}
         </div>
       </div>
     </>
+  }
+
+  onRandomAlbumClick = async (event: MouseEvent) => {
+    const albums = event.altKey ? await getAlbums() : this.props.artistAlbums.albums
+    const album = albums[Math.floor(Math.random() * albums.length)]
+    playAlbum(album)
   }
 
 }
